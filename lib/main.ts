47f18baa21d8fa640c33522fa28f3e40633b274c
LinHanLove <2188817393@qq.com>
2024-05-02 20:37:21 +0800

    feat: import wechat

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,10 @@
 export * from '@/utils/Array/index';
 export * from '@/utils/Browser/index';
-export * from '@/utils/Date/index';
+export * from '@/utils/FormatDate/index';
+export * from '@/utils/HighOrderFunction/index';
 export * from '@/utils/TypeCheck/index';
 export * from '@/utils/Number/index';
 export * from '@/utils/String/index';
+export * from '@/utils/Object/index';
+export * from '@/utils/WeChat/index';
 
