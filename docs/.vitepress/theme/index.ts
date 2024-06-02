9d77835738a941e5071817ec661693030644644c
LinHan <2188817393@qq.com>
2024-06-02 21:51:13 +0800

    feat: fix not permitted

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,5 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import '../../src/public/style/theme.css'
-// import Preview from '../../src/components/Preview.vue'
 
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // app.component('Preview', Preview)
-  }
-}
+export default DefaultTheme
